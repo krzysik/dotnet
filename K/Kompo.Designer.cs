f83d7f1d730af16af2e62544f6e2f626caf30ac1
Krzysik <krzysikbck@gmail.com>
2020-04-10 13:04:31 +0200

    Ustawianie zakresów , dźwięk, obracanie obrazka,ustawianie jednostek
    Ustawianie zakresów , dźwięk, obracanie obrazka,ustawianie jednostek

---
+++ K/Kompo.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace K
+{
+    partial class Kompo
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl0 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Image = global::K.Properties.Resources.gora;
+            this.label1.Location = new System.Drawing.Point(226, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 150);
+            this.label1.TabIndex = 0;
+            // 
+            // lbl0
+            // 
+            this.lbl0.AutoSize = true;
+            this.lbl0.Location = new System.Drawing.Point(288, 113);
+            this.lbl0.Name = "lbl0";
+            this.lbl0.Size = new System.Drawing.Size(13, 13);
+            this.lbl0.TabIndex = 1;
+            this.lbl0.Text = "0";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(386, 113);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(13, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(386, 185);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(13, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(386, 278);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(13, 13);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "0";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(288, 278);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(13, 13);
+            this.lbl4.TabIndex = 5;
+            this.lbl4.Text = "0";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(193, 278);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(13, 13);
+            this.lbl5.TabIndex = 6;
+            this.lbl5.Text = "0";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(193, 195);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(13, 13);
+            this.lbl6.TabIndex = 7;
+            this.lbl6.Text = "0";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(193, 113);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(13, 13);
+            this.lbl7.TabIndex = 8;
+            this.lbl7.Text = "0";
+            // 
+            // Kompo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lbl0);
+            this.Controls.Add(this.label1);
+            this.Name = "Kompo";
+            this.Size = new System.Drawing.Size(696, 475);
+            this.Load += new System.EventHandler(this.Kompo_Load);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Kompo_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl0;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+    }
+}
---
+++ K/Kompo.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using K.Properties;
+using System.Media;
+
+namespace K
+{
+    public partial class Kompo : UserControl
+    {
+        int x = 0;
+        private double min, max, roznica, scale;
+        private double maxScale, minScale;
+        private Label labelmin, labelmax,labeljed;
+        private String jednostka;
+        String[] images = { "gora","prawogora","prawo","prawodol","dol","lewodol","lewo","lewogora" };
+        public Kompo()
+        {
+            InitializeComponent();
+            Label label1 = new Label();
+            Label lbl0 = new Label();
+            Label lbl1 = new Label();
+            Label lbl2 = new Label();
+            Label lbl3 = new Label();
+            Label lbl4 = new Label();
+            Label lbl5 = new Label();
+            Label lbl6 = new Label();
+            Label lbl7 = new Label();
+            labeljed = new Label();
+             labelmin = new Label();
+             labelmax = new Label();
+            label1.Image = (Bitmap)Resources.ResourceManager.GetObject(images[0]);
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Kompo_MouseWheel);
+
+           
+            label1.Focus();
+
+
+
+
+        }
+      
+        private void Kompo_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+       
+
+      
+
+        private void Kompo_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+           
+            if (e.KeyData == Keys.Up)
+            {
+                if (x == images.Length-1)
+                {
+
+                }
+                else
+                {
+                    x++;
+                    
+                    label1.Image = (Bitmap)Resources.ResourceManager.GetObject(images[x]);
+                    SoundPlayer audio = new SoundPlayer(K.Properties.Resources.klik); 
+                    audio.Play();
+                }
+
+            }
+            else if (e.KeyData == Keys.Down)
+            {
+                if (x == 0)
+                {
+
+                }
+                else
+                {
+                    x--;
+                   
+                    label1.Image = (Bitmap)Resources.ResourceManager.GetObject(images[x]);
+                    SoundPlayer audio = new SoundPlayer(K.Properties.Resources.klik); 
+                    audio.Play();
+                }
+
+            }
+        }
+        private void Kompo_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                if (x == images.Length - 1)
+                {
+
+                }
+                else
+                {
+                    x++;
+
+                    label1.Image = (Bitmap)Resources.ResourceManager.GetObject(images[x]);
+                    SoundPlayer audio = new SoundPlayer(K.Properties.Resources.klik);
+                    audio.Play();
+                }
+            }
+            else
+            {
+                if (x == 0)
+                {
+
+                }
+                else
+                {
+                    x--;
+
+                    label1.Image = (Bitmap)Resources.ResourceManager.GetObject(images[x]);
+                    SoundPlayer audio = new SoundPlayer(K.Properties.Resources.klik);
+                    audio.Play();
+                }
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        public double MaxScale
+        {
+            get{ return maxScale; }
+            set {
+                maxScale = value;
+                labelmax.Text = maxScale.ToString();
+           }
+           
+            
+        }
+
+
+        public double MinScale
+        {
+            get { return minScale; }
+            set {
+                minScale = value;
+                labelmin.Text = minScale.ToString();
+
+                roznica =Double.Parse(labelmax.Text)-Double.Parse(labelmin.Text);
+                scale = roznica / 8;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Double wynik =  ((i + 1) * scale + Double.Parse(labelmin.Text));
+                    this.Controls["lbl" + i.ToString()].Text  = wynik.ToString()+labeljed.Text;
+                  
+                }
+            }
+        }
+        public string Jednostka
+        {
+            get { return jednostka; }
+            set {
+                jednostka = value;
+                labeljed.Text=jednostka ; }
+        }
+
+        
+    }
+}
